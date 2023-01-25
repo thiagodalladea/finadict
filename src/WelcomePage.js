7d68a74689458a03071dc8f00b8f7abdfb8a1b6e
Thiago Dalla Dea <thiagopdalladea@gmail.com>
2023-01-25 01:19:19 -0300

    First commit: setting up some things. Let's work!

---
+++ src/WelcomePage.js
@@ -0,0 +1,8 @@
+import './global.css';
+import { Header } from './components/WelcomePage/Header.js';
+
+export function WelcomePage() {
+    return (
+        <Header />
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { Routes,Route,BrowserRouter } from 'react-router-dom';
+import { WelcomePage } from './WelcomePage.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <BrowserRouter>
+      <Routes>
+          <Route exact path='/' element={ <WelcomePage /> }/>
+      </Routes>
+  </BrowserRouter>
+, document.getElementById('root'))
