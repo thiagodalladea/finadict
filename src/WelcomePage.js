480a0756f39b1d0dcd25f901f213582b0cc285ab
Thiago Dalla Dea <thiagopdalladea@gmail.com>
2023-01-25 03:17:37 -0300

    Correcting size and padding from Welcome pag First Section

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,11 +1,9 @@
 import './global.css';
-import { Header } from './components/WelcomePage/Header.js';
 import { FirstSection } from './components/WelcomePage/FirstSection.js';
 
 export function WelcomePage() {
     return (
         <>
-            <Header />
             <FirstSection />
         </>
     )
--- src/components/WelcomePage/FirstSection.js
+++ src/components/WelcomePage/FirstSection.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import style from './FirstSection.module.css';
 import calculator from '../../assets/blenders/calculator.png';
+import { Header } from './Header.js';
 
 export function FirstSection() {
     return(
         <div className={ style.firstSection }>
+            <Header />
             <div className={ style.container }>
                 <div className={ style.txtContainer }>
                     <h1>Controle das Finanças na palma da sua mão.</h1>
