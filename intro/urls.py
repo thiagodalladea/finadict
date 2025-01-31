979117e4d712fbdfd359094484a128735c77a2c2
Thiago Dalla Dea <thiagopdalladea@gmail.com>
2025-01-31 18:47:53 -0300

    sign up page complete

--- intro/urls.py
+++ intro/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from intro.views import index, login
+from intro.views import index, login, signup
 
 urlpatterns = [
     path('', index, name="index"),
-    path('/login', login, name="login")
+    path('/login', login, name="login"),
+    path('/signup', signup, name='signup'),
 ]
